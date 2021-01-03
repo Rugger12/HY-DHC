6829a33b362afbae2d44be078dd21ca7be7bdee1
Rugger12 <yhpark81@gmail.com>
2021-01-04 01:58:17 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">ⓒ한양디지털헬스케어센터 <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">ⓒ한양디지털헬스케어센터 <a href="https://hyu.wiki/%ED%95%9C%EC%96%91%EB%94%94%EC%A7%80%ED%84%B8%ED%97%AC%EC%8A%A4%EC%BC%80%EC%96%B4%EC%84%BC%ED%84%B0"></a></p>
     </footer>
 )
 
