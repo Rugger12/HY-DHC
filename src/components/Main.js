dbac7ff83ef41114d9865090b556b7426d6f2b73
Rugger12 <yhpark81@gmail.com>
2021-01-04 02:15:32 +0900

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,40 +33,26 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            한양디지털헬스케어센터
-
-디지털헬스케어센터는 Digital Therapeutics 및 그 응용과 관련된 연구를 체계적으로 수행하여 공학, 행동과학, 뇌신경과학, 임상, 인공지능 기반의 학제간 융합연구를 활성화하고, Digital Therapeutics 데이터셋 구축 및 서비스 기술 제공을 목적으로 2020년 9월 1일 (규정개정) 신설된 대학 서울캠퍼스 부설 연구기관이다.
-
-&gt; 영문명 : HYU Digital Healthcare Center, HY-DHC
-> 중문명 :
-별도 센터 규정 있음
-
-역사
-2020.09.01 신설(규정개정20200901)
-
-사업
-규정에 명시된 사업 내역(2020.09.01)
-
-1. Digital Therapeutics 기초 및 응용 연구를 통한 대학의 연구 경쟁력 제고 및 국제화
-2. Digital Therapeutics 관련 연구과제의 수행을 위한 정보 확보 및 연구과제의 도출
-3. 연구효율 증진을 목적으로 한 연구지원 시설의 관리 및 기타 연구자원의 운용
-4. 연구발표, 세미나 개최 및 이에 따르는 간행물 발간
-5. Digital Therapeutics 관련 학제간 융합연구를 통한 연구인력 양성
-6. 국내외 관련 연구기관과의 협력·협업 연구
-7. 기타 본 디지털헬스케어센터의 연구 목적에 필요한 사업 <a href="#work">awesome work</a>.
+          <a href="#work">awesome work</a>.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+              한양디지털헬스케어센터
+              디지털헬스케어센터는 Digital Therapeutics 및 그 응용과 관련된 연구를 체계적으로 수행하여 
+              공학, 행동과학, 뇌신경과학, 임상, 인공지능 기반의 학제간 융합연구를 활성화하고, 
+              Digital Therapeutics 데이터셋 구축 및 서비스 기술 제공을 목적으로 
+              2020년 9월 1일 (규정개정) 신설된 대학 서울캠퍼스 부설 연구기관이다.
+
+              > 영문명 : HYU Digital Healthcare Center, HY-DHC
+
+              [역사] 2020.09.01 신설(규정개정20200901)
+              [사업] 규정에 명시된 사업 내역(2020.09.01)
+                1. Digital Therapeutics 기초 및 응용 연구를 통한 대학의 연구 경쟁력 제고 및 국제화
+                2. Digital Therapeutics 관련 연구과제의 수행을 위한 정보 확보 및 연구과제의 도출
+                3. 연구효율 증진을 목적으로 한 연구지원 시설의 관리 및 기타 연구자원의 운용
+                4. 연구발표, 세미나 개최 및 이에 따르는 간행물 발간
+                5. Digital Therapeutics 관련 학제간 융합연구를 통한 연구인력 양성
+                6. 국내외 관련 연구기관과의 협력·협업 연구
+                7. 기타 본 디지털헬스케어센터의 연구 목적에 필요한 사업Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
           </p>
           {close}
         </article>
