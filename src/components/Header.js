ea787730bebfc4b97d449ec8ead189d319846bcf
Rugger12 <yhpark81@gmail.com>
2021-01-03 16:20:59 +0000

    Header.js: updated Url in Link 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = props => (
         <h1>HYU Digital healthcare center</h1>
         <p>
           Digital Therapeutics 및 그 응용과 관련된 연구를 체계적으로 수행하여 공학, 행동과학, 뇌신경과학, 임상, 인공지능 기반의 학제간 융합연구를 활성화하고, Digital Therapeutics 데이터셋 구축 및 서비스 기술 제공을 목적으로 함{' '}
-          <a href="https://hyu.wiki/%ED%95%9C%EC%96%91%EB%94%94%EC%A7%80%ED%84%B8%ED%97%AC%EC%8A%A4%EC%BC%80%EC%96%B4%EC%84%BC%ED%84%B0">한양디지털헬스케어센터</a>  Digital Therapeutics 데이터셋 구축 및 서비스 기술 제공하기 위해 설립된 한양대학교 서울캠퍼스 부설 연구기관입니다.
+          <a href="Digital Therapeutics 및 그 응용과 관련된 연구를 체계적으로 수행하여 공학, 행동과학, 뇌신경과학, 임상, 인공지능 기반의 학제간 융합연구를 활성화하고, Digital Therapeutics 데이터셋 구축 및 서비스 기술 제공을 목적">한양디지털헬스케어센터</a>  Digital Therapeutics 데이터셋 구축 및 서비스 기술 제공하기 위해 설립된 한양대학교 서울캠퍼스 부설 연구기관입니다.
           <br />
           for free under the{' '}
           <a href="https://html5up.net/license">Creative Commons</a> license.
