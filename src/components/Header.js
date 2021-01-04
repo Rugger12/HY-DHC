e43fbe0c50927692d90367bdf1947245915753d1
Rugger12 <yhpark81@gmail.com>
2021-01-04 14:54:59 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,9 @@ const Header = props => (
       <div className="inner">
         <h1>HYU Digital healthcare center</h1>
         <p>
-          {' '}<a href="https://hyu.wiki/%ED%95%9C%EC%96%91%EB%94%94%EC%A7%80%ED%84%B8%ED%97%AC%EC%8A%A4%EC%BC%80%EC%96%B4%EC%84%BC%ED%84%B0">한양디지털헬스케어센터 </a>는 Digital Therapeutics 및
-          <br/> 그 응용과 관련된 연구를 체계적으로 수행하여 공학, 행동과학, 뇌신경과학, 임상, 인공지능 기반의
-          <br/> 학제간 융합연구를 활성화하고, Digital Therapeutics 데이터셋 구축 및 서비스 기술을 
-          <br/> 제공하기 위해 설립된 {' '}
-          <a href="https://www.hanyang.ac.kr/">한양대학교 서울캠퍼스</a> 부설 연구기관입니다.
+          {' '}<a href="https://hyu.wiki/%ED%95%9C%EC%96%91%EB%94%94%EC%A7%80%ED%84%B8%ED%97%AC%EC%8A%A4%EC%BC%80%EC%96%B4%EC%84%BC%ED%84%B0">한양디지털헬스케어센터 </a>
+          는 Digital Therapeutics 및 그 응용과 관련된 연구를 수행하여 공학, 행동과학, 뇌신경과학, 임상, 인공지능 기반의 학제간 융합연구를 활성화하고, 데이터셋 구축 및 서비스 기술을 
+          제공하기 위해 설립된 {' '}<a href="https://www.hanyang.ac.kr/">한양대학교 서울캠퍼스</a> 부설 연구기관입니다.
         </p>
       </div>
     </div>
